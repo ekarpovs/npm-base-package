d122553784a2762ab95051a11fc5f7cf17aabc75
evgeny <ekarpovsky@gmail.com>
2023-08-27 10:51:35 +0000

    refactor(index, index.test): add goodby

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-export const helloWorld = () => {
-  const greeting = "Hello World";
+export const greet = () => {
+  const greeting = "Hello World!";
   return greeting;
+};
+
+export const goodby = () => {
+  const by = "Goodby!";
+  return by;
 };
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,17 @@
 import {describe, expect, test} from '@jest/globals';
 
-import { helloWorld } from '../src/index';
+import { greet, goodby } from '../src/index';
 
 describe('', () => {
-  it('should say Hello World', () => {
-    const expected = 'Hello World';
-    const result = helloWorld();
+  it('should say Hello World!', () => {
+    const expected = 'Hello World!';
+    const result = greet();
+    expect(result).toBe(expected)});
+});
+
+describe('', () => {
+  it('should say Goodby!', () => {
+    const expected = 'Goodby!';
+    const result = goodby();
     expect(result).toBe(expected)});
 });
